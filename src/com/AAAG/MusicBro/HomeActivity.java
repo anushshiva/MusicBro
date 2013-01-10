60a2dd943a76e209e8307cb9ee03179833b18238
anushshiva <anushshiva@gmail.com>
2013-01-10 22:21:23 +0530

    further designing

--- src/com/AAAG/MusicBro/HomeActivity.java
+++ src/com/AAAG/MusicBro/HomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -53,5 +54,15 @@ public class HomeActivity extends Activity {
                 startActivity(new Intent(HomeActivity.this, AlbumActivity.class));
             }
         });
+    }
+    @Override
+    public boolean onKeyDown(int keyCode,KeyEvent event)
+    {
+        if(keyCode == KeyEvent.KEYCODE_BACK)
+        {
+            startActivity(new Intent(HomeActivity.this, ChoiceActivity.class));
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
     }
 }
